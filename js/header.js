0f04d79443fbeae72714c9437f10ef2371141d2d
Ken <akitakensuke@gmail.com>
2025-08-02 18:31:10 -0700

    Edit

---
+++ js/header.js
@@ -0,0 +1,30 @@
+function generateHeader() {
+	const body = document.querySelector('body');
+
+	const header = document.createElement('header');
+	header.innerHTML = `
+    <nav>
+        <div class="header__logo">
+            <img src="../img/logo/company_logo.png" alt="Company" />
+        </div>
+
+        <ul class="header__nav-links">
+            <li>
+                <a href="#home">Home</a>
+            </li>
+            <li>
+                <a href="#about">About</a>
+            </li>
+            <li>
+                <a href="#services">Services</a>
+            </li>
+            <li>
+                <a href="#gallery">Gallery</a>
+            </li>
+        </ul>
+    </nav>
+    `;
+	body.appendChild(header);
+}
+
+generateHeader();
